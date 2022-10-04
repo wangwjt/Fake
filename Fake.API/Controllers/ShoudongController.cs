3555a59e378edfa9bab6d0298ddd74521e199ef3
wangwjt <wangwjt1121@126.com>
2022-10-04 19:25:17 +0800

    第一个controller

---
+++ Fake.API/Controllers/ShoudongController.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Fake.API.Controllers
+{
+    // 添加路由
+    [Route("test1")]
+    [Controller]
+    public class ShoudongController : Controller
+    {
+        // 为该路由添加一个get方法
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "aaa","bbb"};
+        }
+    }
+}
